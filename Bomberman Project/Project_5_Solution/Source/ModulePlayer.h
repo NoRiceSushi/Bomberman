73a0987f90f54f497bc73c0cea64a66605d6bf7e
Galycon <125640143+Galycon@users.noreply.github.com>
2023-04-17 08:23:33 +0200

    Update ModulePlayer.h

--- Bomberman Project/Project_5_Solution/Source/ModulePlayer.h
+++ Bomberman Project/Project_5_Solution/Source/ModulePlayer.h
@@ -35,6 +35,8 @@ public:
 	// The speed in which we move the player (pixels per frame)
 	int speed = 1;
 
+	int ACT_PUYOS = 0;
+	int MAX_PUYOS=100;
 	// The player spritesheet loaded into an SDL_Texture
 	SDL_Texture* texture = nullptr;
 	
