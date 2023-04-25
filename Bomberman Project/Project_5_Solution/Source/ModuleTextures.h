ee924e96486b58f7fed7ccff45f9f2599f49ff71
Chen075 <jcy123456789h@gmail.com>
2023-04-25 19:16:55 +0200

    Adding unload to textures
    ok?

--- Bomberman Project/Project_5_Solution/Source/ModuleTextures.h
+++ Bomberman Project/Project_5_Solution/Source/ModuleTextures.h
@@ -30,6 +30,8 @@ public:
 	// Returns nullptr if the texture could not be created
 	SDL_Texture* const Load(const char* path);
 
+	bool Unload(SDL_Texture* texture);
+
 public:
 	// An array of all the loaded textures
 	// Allows us to keep track of all textures and free them on application exit
