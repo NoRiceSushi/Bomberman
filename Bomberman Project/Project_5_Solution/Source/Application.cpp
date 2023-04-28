83846530fe93da26071fea04708fb1f35434f91a
Chen075 <jcy123456789h@gmail.com>
2023-04-29 01:14:37 +0200

    adjustment
    mb

--- Bomberman Project/Project_5_Solution/Source/Application.cpp
+++ Bomberman Project/Project_5_Solution/Source/Application.cpp
@@ -27,9 +27,9 @@ Application::Application()
 	modules[4] = players = new ModulePlayers(true);
 	modules[5] = audio = new ModuleAudio(true);
 	modules[6] = particles = new ModuleParticles(true);
-	modules[7] = menu = new ModuleMenu(false);
+	modules[7] = menu = new ModuleMenu(true);
 	modules[8] = fade = new ModuleFade(true);
-	modules[9] = gameover = new ModuleGameOver(false);
+	modules[9] = gameover = new ModuleGameOver(true);
 	modules[9] = player = new ModulePlayer(true);
 	modules[10] = render = new ModuleRender(true);
 
