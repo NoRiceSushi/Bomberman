227b44422e5e4a855bfe1c86b191d48443b8d7cb
Galycon <2004rasr@gmail.com>
2023-05-03 13:27:51 +0200

    Perder

--- Bomberman Project/Project_5_Solution/Source/ModulePlayer.cpp
+++ Bomberman Project/Project_5_Solution/Source/ModulePlayer.cpp
@@ -323,7 +323,9 @@ update_status ModulePlayer::Update()
 				App->scene->SetTile((p[0].position.x - 25) / 16, (p[0].position.y - 32) / 16, p[0].color);
 				App->scene->SetTile((p[1].position.x - 25) / 16, (p[1].position.y - 32) / 16, p[1].color);
 				App->scene->SetTile((p[2].position.x - 25) / 16, (p[2].position.y - 32) / 16, p[2].color);
-				if (App->scene->ReadTile(3, 2) == false || App->scene->ReadTile(4, 2) == false) win = true;
+				if (App->scene->ReadTile(3, 2) == false || App->scene->ReadTile(4, 2) == false) { 
+					win = true;
+					App->fade->EnableOnly(this, (Module*)App->lose); }
 
 			}
 		}
