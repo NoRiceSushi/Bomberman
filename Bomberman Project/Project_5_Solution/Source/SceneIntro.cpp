7e7b0e9e2e7b82ef3c84606dc8dbe717692e8ed9
Chen075 <jcy123456789h@gmail.com>
2023-05-04 16:20:13 +0200

    sceneintro audio fix
    ok

--- Bomberman Project/Project_5_Solution/Source/SceneIntro.cpp
+++ Bomberman Project/Project_5_Solution/Source/SceneIntro.cpp
@@ -28,7 +28,7 @@ bool SceneIntro::Start()
 	bool ret = true;
 
 	bgTexture = App->textures->Load("Assets/StartingScreen.png");
-	App->audio->PlayMusic("Assets/Music/02_Title.ogg", 1.0f); 
+	App->audio->PlayMusic("Assets/audio/02_Title.ogg", 1.0f); 
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
